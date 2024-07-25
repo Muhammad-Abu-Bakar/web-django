0a77318bd35df4220a3ddc138aa74b2367edb983
Muhammad Abubakar <53856065+Muhammad-Abu-Bakar@users.noreply.github.com>
2024-07-25 02:37:33 +0100

    Update manage.py

--- django_web_app/manage.py
+++ django_web_app/manage.py
@@ -4,6 +4,17 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_app.settings')
+
+    if 'runserver' in sys.argv:
+        # Check if a port number is provided
+        if len(sys.argv) > 2:
+            port = sys.argv[2]
+        else:
+            port = '8000'  # Default port
+
+        # Adjust the runserver command to include the port
+        sys.argv[2] = port
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
